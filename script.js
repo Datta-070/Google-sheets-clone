4b6af1e9c7af142b7087b5b67958f6a0d5433944
Datta-070 <chinmaydatt003@gmail.com>
2023-11-29 15:45:26 +0530

    Phase 2 Done

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 const header=document.getElementById("header");
 const snoContainer=document.getElementById("sno");
+const bodyContainer=document.getElementById("body-container");
 
 const columns=27, rows=100;
-for(let i=0;i<columns;i++){
+for(let i=1;i<columns;i++){
     const headCell=document.createElement("div");
     headCell.className="head-cell";
-    if(i != 0){
-        headCell.innerText= String.fromCharCode(64+i);
-    }
+    headCell.innerText= String.fromCharCode(64+i);
     header.appendChild(headCell);
 }
 
@@ -16,4 +15,17 @@ for(let i=1;i<=rows;i++){
     snoCell.innerText= i;
     snoCell.className="sno-cell";
     snoContainer.appendChild(snoCell);
+}
+
+for(let row=1;row<=rows;row++){
+    const rowElement=document.createElement("div");
+    rowElement.className="row";
+
+    for(let col=1;col<columns;col++){
+        const cell=document.createElement("div");
+        cell.className="cell";
+        cell.contentEditable=true;
+        rowElement.appendChild(cell);
+    }
+    bodyContainer.appendChild(rowElement);
 }
